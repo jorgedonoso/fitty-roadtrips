44ac5e088e38efb76de44aeb6b5d0f6aec1fb195
Jorge Donoso <jdevcl@gmail.com>
2026-06-28 22:53:32 -0700

    Add intro section

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function MapClient() {
   return (
     <MapProvider>
       <div className="flex h-screen flex-col bg-zinc-100">
-        <h1 className="text-4xl text-center bg-primary text-white p-2 font-bold font-mono">
+        <h1 className="text-6xl text-center bg-primary text-white p-2 font-bold font-mono">
           50 road trips
         </h1>
         <MapSection />
--- components/GallerySection.tsx
+++ components/GallerySection.tsx
@@ -2,6 +2,7 @@
 
 import RegionGallery from "@/components/domain/RegionGallery";
 import { useMapContext } from "@/src/context/MapContext";
+import Image from "next/image";
 
 export default function GallerySection() {
   const { regionName } = useMapContext();
@@ -11,8 +12,17 @@ export default function GallerySection() {
       {regionName ? (
         <RegionGallery name={regionName} />
       ) : (
-        <div className="flex h-full items-center justify-center text-gray-400 italic">
-          Select a region to explore galleries.
+        <div className="flex flex-col items-center justify-start gap-2 pt-5 text-center text-gray-400">
+          <p className="text-xl font-mono tracking-widest">
+            Select a region to start exploring
+          </p>
+          <Image
+            src="/intro.png"
+            alt="Home illustration"
+            width={556}
+            height={368}
+            className="h-auto max-h-[368px] w-auto"
+          />
         </div>
       )}
     </section>
